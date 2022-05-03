7bf1234a98e88485bc6ef408c513c90d88cc0f8e
duyendtcidm <duyendtcidm@gmail.com>
2022-05-04 06:21:54 +0700

    resize done

--- resize.py
+++ resize.py
@@ -2,22 +2,32 @@ import cv2 as cv
 from cv2 import INTER_AREA
 
 img = cv.imread('Photos/img2.jpg')
-cv.imshow('Dubai', img)
-
-# def rescaleFrame(frame, scale = 3):
-#     width = int(frame.shape[1]*scale)
-#     height = int(frame.shape[0]*scale)
-
-#     dimensions = (width, height)
-
-#     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+# cv.imshow('Dubai', img)
 
 
-def rescaleFrame(frame, scale=0.25):
-    width = int(frame.shape[1]*scale)
-    height = int(frame.shape[0]*scale)
+def rescaleFrame(frame, scale=0.2):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
+img_resized = rescaleFrame(img)
+cv.imshow('Dubai', img_resized)
 cv.waitKey(0)
+
+# capture = cv.VideoCapture('Videos/google.mp4')
+
+# while True:
+#     isTrue, frame = capture.read()
+#     frame_resized = rescaleFrame(frame)
+
+#     # Display origin video
+#     # cv.imshow('Video', frame)
+#     cv.imshow('Video', frame_resized)
+
+#     if cv.waitKey(20) & 0xFF == ord('d'):
+#         break
+
+# capture.release()
+# cv.destroyAllWindows()
