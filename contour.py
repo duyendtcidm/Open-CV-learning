a8f7bd89104ff330f6d0476b23e8e335ef2bcfe8
duyendtcidm <duyendtcidm@gmail.com>
2022-05-08 07:36:53 +0700

    countour done

--- contour.py
+++ contour.py
@@ -6,17 +6,22 @@ img = cv.imread('Photos/mini2.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+blank = np.zeros(img.shape, dtype='uint8')
+
 # blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
 
 # canny = cv.Canny(blur, 125, 175)
 # cv.imshow('Canny', canny)
 
 ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
-cv.imshow('Threshold', thresh)
+# cv.imshow('Threshold', thresh)
 
-contour, hierachies = cv.findContours(
+contours, hierachies = cv.findContours(
     thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
-print(f'{len(contour)} contour(s) found!')
+print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 2)
+cv.imshow('Contours draw', blank)
 
 cv.waitKey(0)
