b430787e1140364bfb1b12670bf4310d33d6e290
duyendtcidm <duyendtcidm@gmail.com>
2022-05-08 08:19:35 +0700

    split and merger image

---
+++ splitmerge.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Photos/mini2.jpg')
+# cv.imshow('Large image', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+b, g, r = cv.split(img)
+
+blue = cv.merge([b, blank, blank])
+green = cv.merge([blank, g, blank])
+red = cv.merge([blank, blank, r])
+
+# cv.imshow('Blue', b)
+# cv.imshow('Green', g)
+# cv.imshow('Red', r)
+cv.imshow('Blue', blue)
+cv.imshow('Green', green)
+cv.imshow('Red', red)
+
+# print(img.shape)
+# print(b.shape)
+# print(g.shape)
+# print(r.shape)
+
+# merge = cv.merge([b, g, r])
+# cv.imshow('Merge image', merge)
+
+cv.waitKey(0)
